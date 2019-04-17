b2cba59c79caba121f13740341a0c71251b050a6
billyzou <yz3352@nyu.edu>
2019-04-17 18:59:04 -0400

    feat(view): add board

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-    extends: ['airbnb-typescript/base'],
+    "extends": ["airbnb-base", "plugin:@typescript-eslint/recommended"],
+    parser: "@typescript-eslint/parser",
+    plugins: [
+        '@typescript-eslint',
+    ],
+    rules: {
+        'no-console': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
+    }
 };
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
-const app = require('express')();
+import path = require('path');
+import express = require('express');
+
+const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = 3000;
+http.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
